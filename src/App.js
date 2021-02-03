4fda4724937a1a212da1f41b52220d34e65cf05c
Reinoptland <reinoptland@gmail.com>
2021-02-03 09:51:25 +0100

    Simple form, log data on submit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log("VALUES:", data);
+
   return (
     <Container
       className="p-1"
       style={{ backgroundColor: "white", height: "200vh" }}
     >
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>First name</label>
-        <input type="text" name="firstName" />
+        <input type="text" name="firstName" ref={register} />
         <label>Last name</label>
-        <input type="text" name="lastName" />
+        <input type="text" name="lastName" ref={register} />
         <label>Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" ref={register} />
+        <input type="submit" />
       </form>
     </Container>
   );
